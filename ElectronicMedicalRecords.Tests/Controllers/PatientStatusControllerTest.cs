e98fc7346a25285ea03fc16d86cfa86337ef3ee7
187PM13956 - Nguyen Pham Hoang Long - K24T-PM1 <long.187pm13956@vanlanguni.vn>
2021-11-10 14:42:45 +0700

    UnitTest MedicationAndDiagnostic

---
+++ ElectronicMedicalRecords.Tests/Controllers/PatientStatusControllerTest.cs
@@ -0,0 +1,156 @@
+﻿using ElectronicMedicalRecords.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ElectronicMedicalRecords.Areas.Admin.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Transactions;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+
+namespace ElectronicMedicalRecords.Tests.Controllers
+{
+    [TestClass]
+    public class PatientStatusControllerTest
+    {
+        CP24Team08Entities db = new CP24Team08Entities();
+        PatientStatusController controller = new PatientStatusController();
+
+        [TestMethod]
+        public void TestIndex()
+        {
+            var list = controller.Index() as ViewResult;
+            Assert.IsNotNull(list);
+
+            var model = list.Model as List<PatientStatu>;
+            Assert.IsNotNull(model);
+
+            Assert.AreEqual(db.PatientStatus.Count(), model.Count);
+        }
+
+        [TestMethod]
+        public void GetData()
+        {
+            var getData = controller.GetData() as JsonResult;
+            Assert.IsNotNull(getData);
+            dynamic check = getData.Data;
+            Assert.AreEqual(db.PatientStatus.Count(), check.data.Count);
+        }
+
+        [TestMethod]
+        public void TestCreateP()
+        {
+            var rand = new Random();
+            var patientStatus = new PatientStatu
+            {
+                Name = rand.ToString()
+            };
+
+            using (var scope = new TransactionScope())
+            {
+                var result = controller.Create(patientStatus) as JsonResult;
+                Assert.IsNotNull(result);
+            }
+
+            patientStatus.Name = "Tử vong";
+            controller.ModelState.Clear();
+            using (var scope = new TransactionScope())
+            {
+                var result1 = controller.Create(patientStatus) as JsonResult;
+                Assert.IsNotNull(result1);
+
+                dynamic noti = result1.Data;
+                Assert.AreEqual(false, noti.success);
+                Assert.AreEqual("Tình trạng bệnh nhân đã có trong danh sách", noti.responseText);
+            }
+
+            patientStatus.Name = null;
+            controller.ModelState.Clear();
+            using (var scope = new TransactionScope())
+            {
+                controller.ModelState.AddModelError("", "Error Message");
+                var result2 = controller.Create(patientStatus) as ViewResult;
+                Assert.IsNotNull(result2);
+            }
+        }
+
+        [TestMethod]
+        public void TestEditG()
+        {
+            var result = controller.Edit(0) as HttpNotFoundResult;
+            Assert.IsNotNull(result);
+
+            var patientStatus = db.PatientStatus.First();
+            var result1 = controller.Edit(patientStatus.ID) as JsonResult;
+            Assert.IsNotNull(result1);
+            dynamic data = result1.Data;
+            Assert.IsNotNull(data.data);
+        }
+
+        [TestMethod]
+        public void TestEditP()
+        {
+            var rand = new Random();
+            var patientStatus = db.PatientStatus.AsNoTracking().FirstOrDefault();
+            patientStatus.Name = rand.ToString();
+            using (var scope = new TransactionScope())
+            {
+                var result = controller.Edit(patientStatus) as JsonResult;
+                Assert.IsNotNull(result);
+                dynamic data = result.Data;
+                Assert.AreEqual(true, data.success);
+            }
+
+            patientStatus.Name = "Tử vong";
+            controller.ModelState.Clear();
+            using (var scope = new TransactionScope())
+            {
+                var result1 = controller.Edit(patientStatus) as JsonResult;
+                Assert.IsNotNull(result1);
+                dynamic data = result1.Data;
+                Assert.AreEqual(false, data.success);
+                Assert.AreEqual("Tình trạng bệnh nhân đã có trong danh sách", data.responseText);
+            }
+
+            patientStatus.Name = null;
+            controller.ModelState.Clear();
+            using (var scope = new TransactionScope())
+            {
+                controller.ModelState.AddModelError("", "Error Message");
+                var result2 = controller.Edit(patientStatus) as ViewResult;
+                Assert.IsNotNull(result2);
+            }
+        }
+
+        [TestMethod]
+        public void TestDeleteG()
+        {
+            var result = controller.Delete(0) as HttpNotFoundResult;
+            Assert.IsNotNull(result);
+
+            var patientStatus = db.PatientStatus.First();
+            var result1 = controller.Delete(patientStatus.ID) as JsonResult;
+            Assert.IsNotNull(result1);
+
+            dynamic noti = result1.Data;
+            Assert.IsNotNull(noti.data);
+        }
+
+        [TestMethod]
+        public void TestDeleteP()
+        {
+            var patientStatus = db.PatientStatus.AsNoTracking().First();
+            using (var scope = new TransactionScope())
+            {
+                var result = controller.DeleteConfirmed(patientStatus.ID) as JsonResult;
+                Assert.IsNotNull(result);
+
+                dynamic noti = result.Data;
+                Assert.AreEqual(true, noti.success);
+            }
+        }
+    }
+}
