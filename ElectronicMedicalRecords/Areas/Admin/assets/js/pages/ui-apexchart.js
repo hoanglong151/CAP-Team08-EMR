8a2d427ea12c4f3eca6faedad1b496869fb7c958
187PM13956 - Nguyen Pham Hoang Long - K24T-PM1 <long.187pm13956@vanlanguni.vn>
2022-01-24 13:58:54 +0700

    Change url chart

--- ElectronicMedicalRecords/Areas/Admin/assets/js/pages/ui-apexchart.js
+++ ElectronicMedicalRecords/Areas/Admin/assets/js/pages/ui-apexchart.js
@@ -2,7 +2,7 @@ var bar = new ApexCharts(document.querySelector("#bar"), barOptions);
 var area = new ApexCharts(document.querySelector("#area"), areaOptions);
 var url = 'http://cntttest.vanlanguni.edu.vn:18080/CP24Team08/Admin/Dashboard/GetData';
 var urlLocal = 'https://localhost:44328/Admin/Dashboard/GetData';
-$.getJSON(urlLocal, function (res) {
+$.getJSON(url, function (res) {
 	bar.updateOptions({
 		series: [{
 			data: res.priceExaminationInfo
@@ -19,7 +19,7 @@ $.getJSON(urlLocal, function (res) {
 	});
 })
 
-$.getJSON(urlLocal, function (res) {
+$.getJSON(url, function (res) {
 	area.updateOptions({
 		series: [{
 			data: res.numUser
